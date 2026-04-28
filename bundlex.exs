75b85f613c9d39463ed912b0e412556bf9d66682
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-04-28 10:48:43 +0200

    Update precompiled dependency provider

--- bundlex.exs
+++ bundlex.exs
@@ -14,8 +14,10 @@ defmodule Membrane.H265.FFmpeg.BundlexProject do
         sources: ["decoder.c"],
         os_deps: [
           ffmpeg: [
-            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg),
-             ["libavcodec", "libavutil"]},
+            {:precompiled,
+             Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg,
+               version: "latest"
+             ), ["libavcodec", "libavutil"]},
             {:pkg_config, ["libavcodec", "libavutil"]}
           ]
         ],
@@ -26,8 +28,10 @@ defmodule Membrane.H265.FFmpeg.BundlexProject do
         sources: ["encoder.c"],
         os_deps: [
           ffmpeg: [
-            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg),
-             ["libavcodec", "libavutil"]},
+            {:precompiled,
+             Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg,
+               version: "latest"
+             ), ["libavcodec", "libavutil"]},
             {:pkg_config, ["libavcodec", "libavutil"]}
           ]
         ],
